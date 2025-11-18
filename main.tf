6af2d441774da18e05c9286e57c0d0bf8152d639
Mladenov <Svetlin.Mladenov@oliverwyman.com>
2025-11-18 12:15:34 +0100

    testing security reviewer workflow

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "demo" {
-  bucket = "ow-c3-demo-bucket-jaswanth"
+  bucket = "ow-c3-demo-bucket-svetlin"
   acl    = "private"
 
   versioning {
@@ -43,8 +43,8 @@ resource "aws_s3_bucket" "demo" {
 resource "aws_s3_bucket_public_access_block" "demo" {
   bucket = aws_s3_bucket.demo.id
 
-  block_public_acls       = true
-  block_public_policy     = true
+  block_public_acls       = false
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
